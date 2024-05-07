53bd6191df8244410cc0bf3b70e388b7e2d50834
Oceane BOUNAN <bounan.oceane@orange.fr>
2024-05-07 17:52:09 +0400

    map and search bar

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -5,4 +5,7 @@ class Pin < ApplicationRecord
 
   validates :location, presence: true
   validates :date, presence: true
+
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 end
