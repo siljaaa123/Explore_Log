3284ab0486c62e16f4637da4cee88231e4ee3445
Maria del Mar Sevilla Escobar <sevillamariadelmar@gmail.com>
2024-05-09 17:28:40 +0200

    cloudinary fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'open-uri'
+
 puts 'Cleaning database'
-Journey.destroy_all
-Pin.destroy_all
 Template.destroy_all
+Pin.destroy_all
+Journey.destroy_all
+User.destroy_all
 puts 'Creating a user, a journey, a pin and a template'
-user = User.create(email: "me@me.com", password: "password", username: "me")
-journey = Journey.create(name: "Ibiza", location: "Spain", start_date: Date.new(2024, 5, 1), end_date: Date.new(2024, 5, 10), completed: false, description: "Bob & Brian honeymoon", user_id: user.id)
-pin = Pin.create(location: "Ibiza", date: Date.new(2024, 5, 7), journey_id: journey.id)
-Template.create!(name: "floral", pin_id: pin.id)
+user = User.create!(email: "me@me.com", password: "password", username: "me")
+journey = Journey.new(name: "Ibiza", location: "Spain", start_date: Date.new(2024, 5, 1), end_date: Date.new(2024, 5, 10), completed: false, description: "Bob & Brian honeymoon", user_id: user.id)
+photo = URI.open("https://res.cloudinary.com/dsqjxikd6/image/upload/v1715267267/SummerHolidays_khutfu.png")
+journey.cover_photo.attach(io: photo, filename: "SummerHolidays.png", content_type: "image/png")
+journey.save!
+# pin = Pin.create(location: "Ibiza", date: Date.new(2024, 5, 7), journey_id: journey.id)
+# Template.create!(name: "floral", pin: pin)
 puts 'Finished'
