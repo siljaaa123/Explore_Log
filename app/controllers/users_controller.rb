5da11abae3684567690e9efa45f6a0e2841e5f1d
Silja Stoicescu <siljastoicescu@gmail.com>
2024-05-10 09:04:45 +0200

    added white pin pic and some other style stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
+  def index
+    @user = current_user
+  end
+
   def show
-    @user = User.find(params[:id])
+    @user = current_user
     # check the name of the template and render the view with the associated name
   end
 end
