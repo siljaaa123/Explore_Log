21a49aa2140d7202543e309dbbb1f2ea370ae589
Silja Stoicescu <siljastoicescu@gmail.com>
2024-05-13 11:37:18 +0200

    edit text in template

--- app/javascript/controllers/templates_controller.js
+++ app/javascript/controllers/templates_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["textInput", "photoInput", "uploadedPhoto", "frame", "textContainer", "button"]
+  static targets = ["textInput", "photoInput", "uploadedPhoto", "frame", "textContainer", "button", "text"]
 
   connect() {
   }
@@ -19,7 +19,26 @@ export default class extends Controller {
 
   handleTextChange() {
     const text = this.textInputTarget.value;
-    this.textContainerTarget.textContent = text;
+    this.textInputTarget.classList.add("d-none")
+    this.textContainerTarget.insertAdjacentHTML("beforeend", "<p data-action='click->templates#changeText'>" + text + "</p>")
     this.buttonTarget.classList.add("d-none")
+  }
+
+  changeText(event) {
+
+    this.textInputTarget.classList.remove("d-none")
+    this.textInputTarget.value = event.currentTarget.innerText
+    event.currentTarget.remove()
+    // save text value into a variable
+    console.log(this.textContainerTarget.innerHTML)
+
+    //const text = this.textContainerTarget.innerHTML;
+    // clears textarea
+   // this.textContainerTarget.innerHTML = '';
+    // see added text value
+    //this.textContainerTarget.insertAdjacentHTML("beforeend", `<textarea id="text-input" placeholder="Enter text here" data-templates-target="textInput">${text}</textarea>`)
+    //this.textInputTarget.appendChild(text);
+
+    this.buttonTarget.classList.remove("d-none")
   }
 }
