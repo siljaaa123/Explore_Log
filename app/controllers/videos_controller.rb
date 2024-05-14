a353b28d425166b3b8127b9b7fdf24e33497c9d4
Silja Stoicescu <siljastoicescu@gmail.com>
2024-05-14 17:06:12 +0200

    video looks fineeee

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -4,63 +4,36 @@ class VideosController < ApplicationController
     generate_frames
   end
 
-  # def show
-  #   @journey = Journey.find(params[:journey_id])
-  # end
-
   def generate_frames
-    api_endpoint = 'https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v12/static/'
-    access_token = ENV.fetch("MAPBOX_API_KEY")
-
-    base_params = {
-      width: 800, # Width of the map image
-      height: 600, # Height of the map image
-      style: 'mapbox://styles/mapbox/satellite-streets-v12', # Map style
-      access_token: access_token # Mapbox access token
-    }
-
-    # Define an array to store animation frames
-    @animation_frames = []
-
-    # Iterate through sorted markers to create animation frames
-    @pins = @journey.pins
-    if @pins.any?
-      # @pin = Pin.find(params[:pin_id])
-      @markers = @pins.geocoded.map do |pin|
-        {
-          lat: pin.latitude,
-          lng: pin.longitude,
-          info_window_html: render_to_string(partial: "pins/info_window", locals: { pin: pin }),
-          marker_html: render_to_string(partial: "pins/marker")
-        }
-      end
-      @markers.each do |pin|
-        # Construct parameters for the map snapshot
-        snapshot_params = {
-          zoom: 10, # Zoom level for this frame (adjust as needed)
-          center: "#{pin[:lng]},#{pin[:lat]}", # Center coordinates for this frame
-          duration: 5 # Duration for this frame (adjust as needed)
-        }
-        # Merge base parameters with snapshot parameters
-        params = base_params.merge(snapshot_params)
-
-        # Construct the URL for the map snapshot
-        @snapshot_url = "#{api_endpoint}#{params[:center]},#{params[:zoom]}/#{params[:width]}x#{params[:height]}?access_token=#{params[:access_token]}"
-
-        # Create an animation frame with the snapshot URL and other parameters
-        frame = {
-          url: @snapshot_url, # URL of the map snapshot
-          duration: params[:duration] # Duration of this frame
-        }
-
-        # Add the frame to the animation frames array
-        @animation_frames << frame
-      end
-    else
-      @journey_location = {
-        lat: @journey.latitude,
-        lng: @journey.longitude
-      }
+    @journey = Journey.find(params[:journey_id])
+    @animation_frames = @journey.pins.map do |pin|
+      template = Grover.new("http://localhost:3000/pins/#{pin.id}", format: 'A4')
+      template.to_png
     end
+    # Directory where the image files will be saved
+    map = Grover.new("http://localhost:3000/journeys/#{@journey.id}/map", format: 'A4')
+    @animation_frames << map.to_png
+    temp_dir = Rails.root.join('tmp', 'images', 'test')
+    FileUtils.mkdir_p(temp_dir) unless Dir.exist?(temp_dir)
+    temp_files = @animation_frames.each_with_index.map do |frame, i|
+      file_path = File.join(temp_dir, "frame_#{i+1}.png")  # Create sequential filenames
+      File.binwrite(file_path, frame)
+      file_path
+    end
+    output_file = Rails.root.join('tmp', 'output_movie.mp4')
+    framerate = 0.5  # Each image shown for 2 seconds
+    input_pattern = File.join(temp_dir, 'frame_%d.png')
+    # Using system call to ffmpeg directly
+    system("ffmpeg -framerate #{framerate} -i '#{input_pattern}' -vcodec libx264 -pix_fmt yuv420p -vf 'scale=1920:1080' '#{output_file}'")
+    @movie = output_file.to_s
+  ensure
+    # Ensure temporary files are cleaned up
+    temp_files.each { |file| File.delete(file) if File.exist?(file) }
   end
 end
+
+# SLIDESHOW VIDEO
+# -----------------
+# 1. screenshot of map
+# 2. screenshot of each template page
+# 3. Add all screenshots, i.e. frames, into @animation_frames
