d552796bbcdadfa92267c6052a9e842abf641938
Lucy Hollingsworth <lucyholl@me.com>
2024-05-15 08:21:01 +0100

    music button removed

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_13_190832) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "action_text_rich_texts", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "body"
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
+  end
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -42,6 +52,30 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_13_190832) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "elements", force: :cascade do |t|
+    t.string "type"
+    t.integer "x_position"
+    t.integer "y_position"
+    t.integer "width"
+    t.integer "height"
+    t.string "content"
+    t.bigint "pin_id", null: false
+    t.integer "rotation"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["pin_id"], name: "index_elements_on_pin_id"
+  end
+
+  create_table "invitations", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "journey_id", null: false
+    t.boolean "accepted", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["journey_id"], name: "index_invitations_on_journey_id"
+    t.index ["user_id"], name: "index_invitations_on_user_id"
+  end
+
   create_table "journeys", force: :cascade do |t|
     t.string "name"
     t.string "location"
@@ -55,6 +89,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_13_190832) do
     t.float "latitude"
     t.float "longitude"
     t.index ["user_id"], name: "index_journeys_on_user_id"
+  end
+
+  create_table "pin_stickers", force: :cascade do |t|
+    t.bigint "pin_id", null: false
+    t.bigint "sticker_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["pin_id"], name: "index_pin_stickers_on_pin_id"
+    t.index ["sticker_id"], name: "index_pin_stickers_on_sticker_id"
   end
 
   create_table "pin_templates", force: :cascade do |t|
@@ -76,6 +119,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_13_190832) do
     t.float "latitude"
     t.float "longitude"
     t.index ["journey_id"], name: "index_pins_on_journey_id"
+  end
+
+  create_table "stickers", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "templates", force: :cascade do |t|
@@ -105,7 +154,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_13_190832) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "elements", "pins"
+  add_foreign_key "invitations", "journeys"
+  add_foreign_key "invitations", "users"
   add_foreign_key "journeys", "users"
+  add_foreign_key "pin_stickers", "pins"
+  add_foreign_key "pin_stickers", "stickers"
   add_foreign_key "pin_templates", "pins"
   add_foreign_key "pin_templates", "templates"
   add_foreign_key "pins", "journeys"
