4d0f32dc01bae2182f6f2fbd3750b74d5bd77a0d
Silja Stoicescu <siljastoicescu@gmail.com>
2024-05-16 17:12:00 +0200

    movie

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,6 +21,7 @@ Rails.application.configure do
   config.hosts.clear
 
 
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
